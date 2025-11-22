b235a0290065483b061abb9dac5ef1b7a8722226
siliconspecter <siliconspecter@outlook.com>
2025-11-22 00:19:42 +0000

    Add command-line executable to exchange a stream of bytes with the GB.

---
+++ source/command_line_executables/applications/streaming_spi_controller.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <libftdi1/ftdi.h>
+#include "../libraries/throw.h"
+#include "../libraries/reopen_as_read_only_binary_or_throw.h"
+#include "../libraries/reopen_as_write_only_binary_or_throw.h"
+#include "../libraries/read_byte_eof_or_throw.h"
+#include "../libraries/exchange_byte_with_gb_or_throw.h"
+#include "../libraries/write_byte_or_throw.h"
+
+int main(const int argc, const char *const *const argv)
+{
+  (void)(argc);
+  (void)(argv);
+
+  reopen_as_read_only_binary_or_throw(stdin);
+  reopen_as_write_only_binary_or_throw(stdout);
+
+  while (1)
+  {
+    const int from_stdin = read_byte_eof_or_throw(stdin);
+
+    if (from_stdin == EOF)
+    {
+      break;
+    }
+    else
+    {
+      write_byte_or_throw(exchange_byte_with_gb_or_throw(from_stdin), stdout);
+    }
+  }
+
+  return 0;
+}
