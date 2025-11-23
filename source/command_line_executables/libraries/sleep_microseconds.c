b2a3cb07d1457964fc59ab08152265fb150527f4
siliconspecter <siliconspecter@outlook.com>
2025-11-23 16:50:22 +0000

    Add Windows implementation.

--- source/command_line_executables/libraries/sleep_microseconds.c
+++ source/command_line_executables/libraries/sleep_microseconds.c
@@ -1,6 +1,15 @@
 #include "sleep_microseconds.h"
-#include <stddef.h>
+
+#ifdef __MINGW32__
+#include <windows.h>
+
+void sleep_microseconds(const unsigned int microseconds)
+{
+  Sleep(microseconds / 1000);
+}
+#else
 #include <sys/select.h>
+#include <stddef.h>
 
 void sleep_microseconds(const unsigned int microseconds)
 {
@@ -9,3 +18,4 @@ void sleep_microseconds(const unsigned int microseconds)
   interval.tv_usec = microseconds % 1000000;
   select(0, NULL, NULL, NULL, &interval);
 }
+#endif
