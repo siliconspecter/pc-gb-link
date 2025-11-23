422cb8a513416354fb1475c535e7814af68867e4
siliconspecter <siliconspecter@outlook.com>
2025-11-23 16:56:56 +0000

    Add missing error handling.

--- source/command_line_executables/libraries/sleep_microseconds.c
+++ source/command_line_executables/libraries/sleep_microseconds.c
@@ -10,12 +10,32 @@ void sleep_microseconds(const unsigned int microseconds)
 #else
 #include <sys/select.h>
 #include <stddef.h>
+#include <errno.h>
+#include "throw.h"
 
 void sleep_microseconds(const unsigned int microseconds)
 {
-  struct timeval interval;
-  interval.tv_sec = microseconds / 1000000;
-  interval.tv_usec = microseconds % 1000000;
-  select(0, NULL, NULL, NULL, &interval);
+  while (1)
+  {
+    struct timeval interval;
+    interval.tv_sec = microseconds / 1000000;
+    interval.tv_usec = microseconds % 1000000;
+
+    switch (select(0, NULL, NULL, NULL, &interval))
+    {
+    case 0:
+      return;
+
+    case -1:
+      if (errno != EINTR)
+      {
+        throw("Failed to sleep (error number %d).", errno);
+      }
+      break;
+
+    default:
+      throw("Unexpected result of select(...).");
+    }
+  }
 }
 #endif
